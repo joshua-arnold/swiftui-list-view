edf92c59123862efb8447b602b9e891f345e472c
Josh Arnold <josh@josharnold.me>
2024-10-13 12:14:37 -0400

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Josh Arnold on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
